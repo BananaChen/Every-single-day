5cc858f483b4829239389488a2979d1c5e1c273b
yiiju <yijuhung0129@gmail.com>
2017-11-24 23:32:58 +0800

    input data into database

---
+++ config.js
@@ -0,0 +1,9 @@
+config = {
+	'mysql': {
+		'user': 'wp2017_groupc',
+		'password': 'day106',
+		'dbname': 'wp2017_group2017',
+	},
+}
+
+module.exports = config;
--- server.js
+++ server.js
@@ -4,6 +4,58 @@ const port = 2266;
 
 app.use(express.static(__dirname+'/public'));
 
-app.listen(port);
+app.listen(port, function(err) {
+    if(!err) console.log("Listening in port " + port);
+});
 
 //hohho
+
+// Please install npm package mysql first
+const config = require('./config');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: config.mysql.user,
+	password: config.mysql.password
+});
+
+connection.connect();
+
+app.get('/get', function(req, res) {
+  //res.send(`<h1>Hello, ${req.query.name} ${req.query.account} ${req.query.email}</h1>`)
+  var username = req.query.name;  
+  var useraccount = req.query.account;
+  var useremail = req.query.email;
+//  var insert = "INSERT INTO `wp2017_groupc`.`user` (name, account, email) VALUES(?,?,?)";
+  connection.query("INSERT INTO `wp2017_groupc`.`user` (name,account,email) VALUES(?,?,?)",[username, useraccount, useremail], function (err, result){
+    if (err){
+      console.log("insert failed!");
+    }
+    else console.log(result);
+  });
+})
+
+//選擇
+/*
+var sel = "SELECT * FROM `wp2017_groupc`.`user` WHERE account='0001'";
+connection.query(sel, (err,result) => {//result??
+    if (err){
+      console.log('selete failed!');
+    }
+    else{
+      console.log("account: "+result);
+      console.log(result);
+    }
+});
+*/
+//delete data in database
+/*
+var del = "DELETE FROM `wp2017_groupc`.`user` WHERE name = 'yiju'";
+connection.query(del, function (err, result) {
+  if (err){
+    console.log('delete failed!');
+  }
+  console.log("Number of records deleted: " + result.affectedRows);
+});
+*/
