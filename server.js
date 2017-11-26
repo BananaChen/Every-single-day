fe43d15ab5de38306378edb730226406fdd27c89
yiiju <yijuhung0129@gmail.com>
2017-11-26 15:57:50 +0800

    modify check password, use crypto at password

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 2266;
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({extended:false});
+const crypto = require('crypto');
 
 app.use(express.static(__dirname+'/public'));
 
@@ -25,9 +26,11 @@ connection.connect();
 app.get('/get', function(req, res) {  
   var useraccount = req.query.account;
   var userpassword = req.query.password;
+  var md5 = crypto.createHash('md5');
+  userpassword = md5.update(userpassword).digest('hex');//加密密碼
   var checkaccount = 0;
-  var check = "SELECT * FROM `wp2017_groupc`.`user` WHERE account = ? AND password = ?";
-  connection.query(check, [useraccount, userpassword], function (err, rows, result){
+  var check = "SELECT * FROM `wp2017_groupc`.`user` WHERE account = ?";
+  connection.query(check, [useraccount], function (err, rows, result){
     if (err){
       console.log("check failed!");
     }
@@ -37,8 +40,11 @@ app.get('/get', function(req, res) {
       }
     }
     if(checkaccount == 1){ 
+      if(rows[useraccount].password == userpassword){
         console.log(rows[useraccount].account);
         res.redirect('home.html')
+      }
+      else res.send(`Your password is wrong`);
     }
     else{
       res.redirect('form_signup.html')
@@ -50,6 +56,10 @@ app.get('/get_signup', function(req, res) {
   var signup_account = req.query.account_signup;
   var signup_password = req.query.password_signup;
   var signup_passcheck = req.query.password_again;
+  var samepass = 0;
+  if(signup_password == signup_passcheck) samepass = 1;
+  var md5 = crypto.createHash('md5');
+  signup_password = md5.update(signup_password).digest('hex');//加密密碼
   var signup_email = req.query.email_signup;
   var insert = "INSERT INTO `wp2017_groupc`.`user` (name, account, password, email) VALUES(?,?,?,?)";
   var check_signup = "SELECT * FROM `wp2017_groupc`.`user` WHERE account = ?";
@@ -64,18 +74,18 @@ app.get('/get_signup', function(req, res) {
       }
     }
     if(newaccount == 0){
-      connection.query(insert,[signup_name, signup_account, signup_password, signup_email], function (err, result){
-        if (err){
-          console.log("insert failed");
-        }
-        else{
-          if(signup_password == signup_passcheck){
-            res.redirect('home.html')
-            console.log(rows[signup_account].account);
+      if(samepass == 1){
+        connection.query(insert,[signup_name, signup_account, signup_password, signup_email], function (err, result){
+          if (err){
+            console.log("insert failed");
           }
-          else res.send(`Your password is wrong`);
-        }
-      });
+          else{
+            res.redirect('home.html')
+            console.log(signup_account);
+          }
+        });
+      }
+      else res.send(`Your password is wrong`);
     }
     else{
       res.send(`The account has already existed`);
@@ -97,7 +107,7 @@ connection.query(sel, (err,result) => {//result??
 */
 //delete data in database
 /*
-var del = "DELETE FROM `wp2017_groupc`.`user` WHERE account = 'a'";
+var del = "DELETE FROM `wp2017_groupc`.`user` WHERE account = 's'";
 connection.query(del, function (err, result) {
   if (err){
     console.log('delete failed!');
