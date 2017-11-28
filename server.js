173dc34c0981c4b5ce89f9831c007cdb94c24546
yiiju <yijuhung0129@gmail.com>
2017-11-28 22:07:40 +0800

    modify person_information

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
+//log in
 app.post('/post',urlencodedParser, function(req, res) {  
   var useraccount = req.body.account;
   var userpassword = req.body.password;
@@ -51,6 +52,7 @@ app.post('/post',urlencodedParser, function(req, res) {
     }
   });
 });
+//sign up
 app.post('/post_signup',urlencodedParser, function(req, res) {  
   var signup_name = req.body.name_signup;
   var signup_account = req.body.account_signup;
@@ -89,6 +91,23 @@ app.post('/post_signup',urlencodedParser, function(req, res) {
     }
     else{
       res.send(`The account has already existed`);
+    }
+  });
+});
+//person_information
+app.post('/post_info',urlencodedParser, function(req, res) {
+  var p_name = req.body.p_name;
+  var p_birthday = req.body.p_birthday;
+  var p_department = req.body.p_department;
+  var p_hobby = req.body.p_hobby;
+  var p_insert = "INSERT INTO `wp2017_groupc`.`person_information` (name, birthday, department, hobb
+  connection.query(p_insert, [p_name, p_birthday,p_department, p_hobby], function (err, rows, result
+    if (err){
+      console.log("select failed");
+    }
+    else{
+      res.redirect('home.html')
+      console.log(p_name);
     }
   });
 });
