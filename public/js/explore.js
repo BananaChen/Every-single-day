50d2a4cc6d54f019346230db883173d292bb23fc
jens0306 <ace020771@gmail.com>
2017-11-29 18:32:25 +0800

    sfgjs

--- public/js/explore.js
+++ public/js/explore.js
@@ -58,6 +58,7 @@ $(document).ready(function() {
   		onNext: function(element){},                // Callback when the lightbox gallery goes to next item
   		errorMessage: 'The requested content cannot be loaded. Please try again later.' // Error message when content can't be loaded
   	});
+    }
   });
 //infinite scroll
 
