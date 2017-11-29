1e95545b748ed737e6fa6f70f9adcac5e193e9be
jens0306 <ace020771@gmail.com>
2017-11-29 19:02:17 +0800

    sfnk

--- public/js/explore.js
+++ public/js/explore.js
@@ -14,7 +14,13 @@ $(document).ready(function() {
           <div class="wow bounceInUp" data-wow-delay="0.1s">
             <div class="col-md-2 col-sm-12">
               <div class="section-title">
-                <h2 class="head-title">data</h2>
+                <h2 class="head-title">
+                `
+                +
+                data
+                +
+                `
+                </h2>
                 <img src="https://i.imgur.com/xdGPi1p.png" alt="" class="img-responsive">
                 <p class="sec-para">Maybe some bio here? </p>
               </div>
