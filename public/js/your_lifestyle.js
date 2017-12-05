8fe6e6c147a42b709dd80508094c07bb9f243e45
BananaChen <chenssuchi09@gmail.com>
2017-12-05 15:32:39 +0800

    my lifestyle hover little man

--- public/js/your_lifestyle.js
+++ public/js/your_lifestyle.js
@@ -1,22 +1,29 @@
-/*$("#uploadImage").change(function(){
-  readImage( this );
-});
+var hover_or_not;
 
-function readImage(input) {
-  if ( input.files && input.files[0] ) {
-    var FR= new FileReader();
-    FR.onload = function(e) {
-      //e.target.result = base64 format picture
-      $('#img').attr( "src", e.target.result );
-    };
-    FR.readAsDataURL( input.files[0] );
-  }
-}*/
+$(document).ready(function(){
 
-window.fbAsyncInit = function() {       
+  $("div.col-md-5.col-sm-12, #pic_section").hover(
+      function(){
+         $('#small_man').attr("src", "https://i.imgur.com/jzGdnXe.png");
+         $('#pic_section').css("display", "block");
+         $('#upload_section').css("display", "none");
+         hover_or_not = 1;
+      },
+      function() {
+        $('#small_man').attr("src", "https://i.imgur.com/BcFGhlW.png");
+        $('#pic_section').css("display", "none");
+        $('#upload_section').css("display", "block");
+        hover_or_not = 0;
+      }
+  )
+
+
+
+})
+window.fbAsyncInit = function() {
     FB.init({
         appId      : '520313951662227',
-        cookie     : true,  // enable cookies to allow the server to access 
+        cookie     : true,  // enable cookies to allow the server to access
         // the session
         xfbml      : true,  // parse social plugins on this page
         version    : 'v2.8' // use graph api version 2.8
@@ -31,7 +38,7 @@ FB.getLoginStatus(function(response) {
 // Load the SDK asynchronously
 (function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) 
+    if (d.getElementById(id))
         return;
     js = d.createElement(s); js.id = id;
     js.src = "//connect.facebook.net/en_US/sdk.js";
