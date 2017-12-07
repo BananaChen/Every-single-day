3d13a2c9777e28351d67f64096806773bee0badc
jens0306 <ace020771@gmail.com>
2017-12-07 16:24:22 +0800

    Small modification

--- public/js/custom.js
+++ public/js/custom.js
@@ -275,9 +275,9 @@ $("document").ready(function(){
   $.ajax({
     method: "POST",
     url: "user",
-    success : function(data){
-      var text = `<li><a "href="your_lifestyle.html">${data}</a></li>`;
-      $("#yourname").append(text);
+    success : function(data) {
+      var text = `<li><a href="your_lifestyle.html">${data}</a></li>`;
+      $("#yourname").prepend(text);
     }
   }); 
 });
